99e4833b9daa42b74eb5a91233683504ab496576
Orel Zamler <orelzx13@gmail.com>
2024-04-17 17:24:35 +0300

    Finished database.py & changed models.py a bit
    Created a singleton design database instance.
    
    Created models.py containing the models of the project.
    Webserver - enables adding webservers
    RequestHistory - containing the checks history of all web servers

---
+++ database.py
@@ -0,0 +1,18 @@
+from threading import Lock
+from flask_sqlalchemy import SQLAlchemy
+
+
+"""
+    A singleton class responsible for managing the SQLAlchemy database instance.
+"""
+class DatabaseManager:
+    _db = None
+    _lock = Lock()  # Lock for thread safety
+
+    @classmethod
+    def get_db(cls):
+        if cls._db is None:
+            with cls._lock:  # Acquire the lock to ensure thread safety
+                if cls._db is None:  # Double-checking for race conditions
+                    cls._db = SQLAlchemy()
+        return cls._db
---
+++ models.py
@@ -0,0 +1,76 @@
+#  Importing the SQLAlchemy extension for Flask, which provides integration between Flask and SQLAlchemy,
+#  allowing us to work with databases in your Flask application.
+from flask_sqlalchemy import SQLAlchemy
+
+# Helps in handling database-related errors.
+from sqlalchemy.exc import SQLAlchemyError
+from datetime import datetime
+
+
+# Initialize db
+from database import DatabaseManager
+
+
+db = DatabaseManager.get_db()
+
+
+"""
+    Database model for storing information about web servers.
+
+    * Inherits from db.Model, which is a base class provided
+      by SQLAlchemy for database models in Flask applications.
+"""
+class Webserver(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    http_url = db.Column(db.String(255), nullable=False, unique=True)
+    status = db.Column(db.String(20), default='Unknown')
+    last_checked = db.Column(db.DateTime, default=datetime.utcnow)
+
+    
+    # TODO - Decide if I want to define a unique constraint on http_url here or in gui
+    """
+        __table_args__ = (
+        UniqueConstraint('http_url', name='uq_http_url')
+        )
+    """
+
+
+    # Save the current Webserver instance to the database.
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            raise e
+        
+    # Update the status of the Webserver instance.
+    def update_status(self, new_status):
+        try:
+            self.status = new_status
+            db.session.commit()
+        except SQLAlchemyError:
+            db.session.rollback()
+            raise
+
+
+"""
+    Database model for storing request history related to web servers.
+"""
+class RequestHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    webserver_id = db.Column(db.Integer, db.ForeignKey('webserver.id'), nullable=False)
+    response_code = db.Column(db.Integer)
+    latency = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    
+    # Save the current RequestHistory instance to the database.
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            raise e
